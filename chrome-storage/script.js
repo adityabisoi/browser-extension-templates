39d1ea8279e97943caf8fca8e8eb408d76659390
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-05-21 20:20:40 +0530

    chrome storage set and get data completed

--- chrome-storage/script.js
+++ chrome-storage/script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('saveBtn').onclick = function(){
         var saveText = document.getElementById('text').value
-        
+
         chrome.storage.sync.set({'savedText':saveText})
 
+    }
+    document.getElementById("getBtn").onclick = function(){
         chrome.storage.sync.get(['savedText'],function(data){
             alert("Value is "+data.savedText)
         })
